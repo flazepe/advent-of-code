b6e51c4d1da2e845224f9d5eaa0998d45825237d
flazepe <flazepe@gmail.com>
2022-12-05 00:49:20 +0800

    Cleanup

--- 2022/04/index.js
+++ 2022/04/index.js
@@ -7,14 +7,12 @@ console.log(
 			(_acc, _cur) => {
 				const ranges = _cur.split(",").map(_pair => {
 					_pair = _pair.split("-");
-					let range = [];
+					const range = [];
 					for (let i = +_pair[0]; i <= _pair[1]; i++) range.push(i);
 					return range;
 				});
-
 				if (ranges[0].every(_entry => ranges[1].includes(_entry)) || ranges[1].every(_entry => ranges[0].includes(_entry))) _acc["Part 1"]++;
 				if (ranges[0].some(_entry => ranges[1].includes(_entry)) || ranges[1].some(_entry => ranges[0].includes(_entry))) _acc["Part 2"]++;
-
 				return _acc;
 			},
 			{ "Part 1": 0, "Part 2": 0 }
