01a757e7b162fc5f3d9d0709933e8139016a661d
flazepe <flazepe@gmail.com>
2022-12-06 19:00:35 +0800

    Cleanup

--- 2022/06/index.js
+++ 2022/06/index.js
@@ -1,12 +1,15 @@
 const input = require("fs").readFileSync("input.txt", "utf8");
 
 function findUniquePacket(length) {
-	for (let i = 0; i < input.length; i++) {
-		const packet = Array(length)
-			.fill(0)
-			.map((_, _index) => input[i + _index]);
-		if (new Set(packet).size === length) return i + length;
-	}
+	for (let i = 0; i < input.length; i++)
+		if (
+			new Set(
+				Array(length)
+					.fill(0)
+					.map((_, _index) => input[i + _index])
+			).size === length
+		)
+			return i + length;
 }
 
 console.log("Part 1:", findUniquePacket(4));
