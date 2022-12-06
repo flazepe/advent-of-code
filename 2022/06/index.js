2436b9976482e07432b05615f48ed7b60c15df6d
flazepe <flazepe@gmail.com>
2022-12-06 18:32:00 +0800

    Day 6

---
+++ 2022/06/index.js
@@ -0,0 +1,13 @@
+const input = require("fs").readFileSync("input.txt", "utf8").trim();
+
+function findUniquePacket(length) {
+	for (let i = 0; i < input.length; i++) {
+		const packet = Array(length)
+			.fill(0)
+			.map((_, _index) => input[i + _index]);
+		if (new Set(packet).size === length) return i + length;
+	}
+}
+
+console.log("Part 1:", findUniquePacket(4));
+console.log("Part 2:", findUniquePacket(14));
